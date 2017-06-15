21eb188bf658520863a9f63e80ce525f117c62b0
Allen <allenjimenezg@gmail.com>
2017-06-15 11:22:15 -0400

    Fixed merge conflict

--- src/TicketitServiceProvider.php
+++ src/TicketitServiceProvider.php
@@ -16,7 +16,6 @@ use Kordy\Ticketit\Models\Agent;
 use Kordy\Ticketit\Models\Comment;
 use Kordy\Ticketit\Models\Setting;
 use Kordy\Ticketit\Models\Ticket;
-use \Voxels\Voxels;
 
 class TicketitServiceProvider extends ServiceProvider
 {
@@ -62,10 +61,6 @@ class TicketitServiceProvider extends ServiceProvider
                 $codemirror_theme = Setting::grab('codemirror_theme');
                 $view->with(compact('master', 'email', 'tools', 'editor_enabled', 'codemirror_enabled', 'codemirror_theme'));
             });
-            $template = "index";
-            $voxelsInjector = new Voxels($template);
-            $voxelsInjector->injectCSSFile();
-            $voxelsInjector->injectJSCode();
 
             //inlude font awesome css or not
             view()->composer('ticketit::shared.assets', function ($view) {
